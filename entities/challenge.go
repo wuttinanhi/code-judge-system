f9a70076de3a9c6cf41533bf2edf41ca112f69b8
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2023-12-19 18:22:04 +0700

    Remove unique constraint on Challenge name field

--- entities/challenge.go
+++ entities/challenge.go
@@ -2,7 +2,7 @@ package entities
 
 type Challenge struct {
 	ChallengeID uint                `json:"challenge_id" gorm:"primaryKey"`
-	Name        string              `json:"name" gorm:"unique"`
+	Name        string              `json:"name"`
 	Description string              `json:"description"`
 	UserID      uint                `json:"user_id"`
 	User        User                `json:"user" gorm:"foreignKey:UserID"`
