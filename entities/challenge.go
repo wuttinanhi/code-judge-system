b8193ddee590b74ac58923e47298d37ab4503ee2
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2023-12-19 18:28:17 +0700

    Add validation and DTO structs for challenge and challenge testcase

--- entities/challenge.go
+++ entities/challenge.go
@@ -1,10 +1,56 @@
 package entities
 
+import "github.com/gofiber/fiber/v2"
+
 type Challenge struct {
 	ChallengeID uint                `json:"challenge_id" gorm:"primaryKey"`
-	Name        string              `json:"name"`
-	Description string              `json:"description"`
+	Name        string              `json:"name" validate:"required"`
+	Description string              `json:"description" validate:"required"`
 	UserID      uint                `json:"user_id"`
 	User        User                `json:"user" gorm:"foreignKey:UserID"`
 	Testcases   []ChallengeTestcase `json:"testcases" gorm:"foreignKey:ChallengeID"`
+}
+
+type ChallengeCreateDTO struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func ValidateChallengeCreateDTO(c *fiber.Ctx) ChallengeCreateDTO {
+	var dto ChallengeCreateDTO
+
+	if err := c.BodyParser(&dto); err != nil {
+		panic(err)
+	}
+
+	if err := validate.Struct(&dto); err != nil {
+		panic(err)
+	}
+
+	return dto
+}
+
+type ChallengeUpdateDTO struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func ValidateChallengeUpdateDTO(c *fiber.Ctx) ChallengeUpdateDTO {
+	var dto ChallengeUpdateDTO
+
+	if err := c.BodyParser(&dto); err != nil {
+		panic(err)
+	}
+
+	if err := validate.Struct(&dto); err != nil {
+		panic(err)
+	}
+
+	return dto
+}
+
+type ChallengeCreateResponse struct {
+	ChallengeID uint   `json:"challenge_id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
--- entities/challengetestcase.go
+++ entities/challengetestcase.go
@@ -1,9 +1,57 @@
 package entities
 
+import "github.com/gofiber/fiber/v2"
+
 type ChallengeTestcase struct {
 	TestcaseID     uint      `json:"testcase_id" gorm:"primaryKey"`
 	Input          string    `json:"input"`
 	ExpectedOutput string    `json:"expected_output"`
 	ChallengeID    uint      `json:"challenge_id"`
 	Challenge      Challenge `json:"challenge" gorm:"foreignKey:ChallengeID"`
+}
+
+type ChallengeTestcaseCreateDTO struct {
+	Input          string `json:"input" validate:"required"`
+	ExpectedOutput string `json:"expected_output" validate:"required"`
+	ChallengeID    uint   `json:"challenge_id" validate:"required"`
+}
+
+func ValidateChallengeTestcaseCreateDTO(c *fiber.Ctx) ChallengeTestcaseCreateDTO {
+	var dto ChallengeTestcaseCreateDTO
+
+	if err := c.BodyParser(&dto); err != nil {
+		panic(err)
+	}
+
+	if err := validate.Struct(&dto); err != nil {
+		panic(err)
+	}
+
+	return dto
+}
+
+type ChallengeTestcaseUpdateDTO struct {
+	Input          string `json:"input"`
+	ExpectedOutput string `json:"expected_output"`
+}
+
+func ValidateChallengeTestcaseUpdateDTO(c *fiber.Ctx) ChallengeTestcaseUpdateDTO {
+	var dto ChallengeTestcaseUpdateDTO
+
+	if err := c.BodyParser(&dto); err != nil {
+		panic(err)
+	}
+
+	if err := validate.Struct(&dto); err != nil {
+		panic(err)
+	}
+
+	return dto
+}
+
+type ChallengeTestcaseCreateResponse struct {
+	TestcaseID     uint   `json:"testcase_id"`
+	Input          string `json:"input"`
+	ExpectedOutput string `json:"expected_output"`
+	ChallengeID    uint   `json:"challenge_id"`
 }
