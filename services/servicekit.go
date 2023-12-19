7104568a85ddaa5d6d479b549a2be8d4728d026d
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2023-12-19 18:21:59 +0700

    Add challenge and submission services to ServiceKit

--- services/servicekit.go
+++ services/servicekit.go
@@ -11,12 +11,16 @@ import (
 var SERVICE_KIT *ServiceKit
 
 type ServiceKit struct {
-	JWTService  JWTService
-	UserService UserService
+	JWTService        JWTService
+	UserService       UserService
+	ChallengeService  ChallengeService
+	SubmissionService SubmissionService
 }
 
 func newServiceKit(db *gorm.DB) *ServiceKit {
 	userRepo := repositories.NewSQLUserRepository(db)
+	challengeRepo := repositories.NewChallengeRepository(db)
+	submissionRepo := repositories.NewSubmissionRepository(db)
 
 	// read env var "JWT_SECRET" and pass it to JWTService
 	// if JWT_SECRET is empty, use default value
@@ -27,10 +31,14 @@ func newServiceKit(db *gorm.DB) *ServiceKit {
 
 	jwtService := NewJWTService(jwtSecret)
 	userService := NewUserService(userRepo)
+	challengeService := NewChallengeService(challengeRepo)
+	submissionService := NewSubmissionService(submissionRepo)
 
 	return &ServiceKit{
-		JWTService:  jwtService,
-		UserService: userService,
+		JWTService:        jwtService,
+		UserService:       userService,
+		ChallengeService:  challengeService,
+		SubmissionService: submissionService,
 	}
 }
 
