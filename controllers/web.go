5002518df209a78fb832c20de24f024ca4722821
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2023-12-27 01:51:08 +0700

    Remove commented out code for challenge testcases

--- controllers/web.go
+++ controllers/web.go
@@ -26,7 +26,6 @@ func SetupWeb(serviceKit *services.ServiceKit) *fiber.App {
 
 	authHandler := NewAuthHandler(serviceKit)
 	challengeHandler := NewChallengeHandler(serviceKit)
-	// challengeTestcaseHandler := NewChallengeTestcaseHandler(serviceKit)
 	submissionHandler := NewSubmissionHandler(serviceKit)
 
 	userGroup := app.Group("/user")
@@ -40,13 +39,6 @@ func SetupWeb(serviceKit *services.ServiceKit) *fiber.App {
 	challengeGroup.Get("/get/:id", challengeHandler.GetChallengeByID)
 	challengeGroup.Put("/update/:id", challengeHandler.UpdateChallenge)
 	challengeGroup.Delete("/delete/:id", challengeHandler.DeleteChallenge)
-
-	// testcaseGroup := app.Group("/testcase")
-	// testcaseGroup.Use(UserMiddleware(serviceKit))
-	// testcaseGroup.Post("/create", challengeTestcaseHandler.CreateTestcase)
-	// testcaseGroup.Get("/get/:id", challengeTestcaseHandler.GetTestcaseByID)
-	// testcaseGroup.Put("/update", challengeTestcaseHandler.UpdateTestcase)
-	// testcaseGroup.Delete("/delete/:id", challengeTestcaseHandler.DeleteTestcase)
 
 	submissionGroup := app.Group("/submission")
 	submissionGroup.Use(UserMiddleware(serviceKit))
